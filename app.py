233d81f1589cff1ad2c3deecb0126b7bb44956c5
egorcry <abulapin@mail.ru>
2023-05-06 23:20:35 +0300

    Added some new urls for workers and admins

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import mysql.connector
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, redirect, url_for, abort
 
 app = Flask(__name__)
+
+absolute_id = -1
 
 # параметры подключения к базе данных
 config = {
@@ -17,7 +19,7 @@ cursor = cnx.cursor()
 
 @app.route('/')
 def main():
-    return jsonify({'Title': 'Main Page'})
+    return jsonify({'Title': 'Main Page', 'ID_worker': absolute_id})
 
 @app.route('/workers')
 def get_workers():
@@ -28,6 +30,11 @@ def get_workers():
 
 @app.route('/registration', methods=['POST'])
 def registration():
+    global absolute_id
+    
+    if absolute_id != -1:
+        return jsonify({'message': 'There is already user in the system'})
+
     login = request.form['login']
     password = request.form['password']
 
@@ -39,24 +46,58 @@ def registration():
         # Если пользователь не найден, вернуть сообщение об ошибке
         return jsonify({'error': 'Invalid login or password'}), 401
 
+    absolute_id = account[0]
+
     query = 'SELECT * FROM workers WHERE ID_worker = %s'
-    id = account[0]
-    cursor.execute(query, (id,))
+    cursor.execute(query, (absolute_id,))
     result = cursor.fetchone()
     
-    if result[5] == 'Admin':
+    if result[7]:
         return redirect(url_for('admin'))
     return redirect(url_for('worker'))
 
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    return jsonify({'message': 'Hello, Admin!'})
+    global absolute_id
+
+    query = 'SELECT Responsible FROM workers WHERE ID_worker = %s'
+    cursor.execute(query, (absolute_id,))
+    result = cursor.fetchone()
+
+    if not result[0]:
+        abort(403, 'Forbidden')
+
+    if request.method == 'POST' and request.form.get('action') == 'exit':
+        return redirect(url_for('logout'))
+    else:
+        return jsonify({'message': 'Hello, Admin!', 'ID_worker': absolute_id})
 
 
-@app.route('/worker')
+@app.route('/worker', methods=['GET', 'POST'])
 def worker():
-    return jsonify({'message': 'Hello, Worker!'})
+    global absolute_id
+
+    query = 'SELECT Responsible FROM workers WHERE ID_worker = %s'
+    cursor.execute(query, (absolute_id,))
+    result = cursor.fetchone()
+
+    if result[0]:
+        abort(403, 'Forbidden')
+
+    if request.method == 'POST' and request.form.get('action') == 'exit':
+        return redirect(url_for('logout'))
+    else:
+        return jsonify({'message': 'Hello, Admin!', 'ID_worker': absolute_id})
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    global absolute_id
+    if absolute_id == -1:
+        return jsonify({'message': 'There is no user in the system'})
+    absolute_id = -1
+    return jsonify({'message': 'Logged out successfully'})
 
 
 if __name__ == '__main__':
